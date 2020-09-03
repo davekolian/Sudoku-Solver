0aa00d0ba6f703068d0bda688ac9491725e2c59f
davekolian <davekolian@users.noreply.github.com>
2020-09-03 23:30:22 +0200

    LOTS OF CLEANING

--- src/code/Node.java
+++ src/code/Node.java
@@ -6,34 +6,16 @@ import java.util.List;
 public class Node {
     private State state;
     private Node parent;
-    private int level;
 
-    public Node(State state, Node parent, int level) {
+
+    public Node(State state, Node parent) {
         this.state = state;
         this.parent = parent;
-        this.level = level;
     }
 
-    public static List<Node> prevNode(List<Node> origList) {
-        try {
-            List<Node> clone = new ArrayList<>();
-
-            for (Node node : origList) {
-                clone.add((Node) node.clone());
-            }
-
-            return clone;
-        } catch (CloneNotSupportedException e) {
-            throw new InternalError(e);
-        }
-    }
 
     public State getState() {
         return state;
-    }
-
-    public void setState(State state) {
-        this.state = state;
     }
 
     public Node getParent() {
@@ -44,11 +26,4 @@ public class Node {
         this.parent = parent;
     }
 
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
 }
