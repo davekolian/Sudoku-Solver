314fb622ec881060d67a8e2dc0bfd74a76fdcb7b
davekolian <davekolian@users.noreply.github.com>
2020-09-05 15:38:26 +0200

    Init of GUI (JavaFX)

---
+++ src/code/Main.java
@@ -0,0 +1,21 @@
+package code;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sudokuWindow.fxml"));
+        stage.setTitle("Sudoku Solver");
+        stage.setScene(new Scene(root, 800, 800));
+        stage.show();
+    }
+
+    public static void main(String[] args){
+        launch(args);
+    }
+}
---
+++ src/code/WindowController.java
@@ -0,0 +1,13 @@
+package code;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class WindowController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
