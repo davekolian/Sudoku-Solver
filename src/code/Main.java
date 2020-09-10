6459932c61a15629cd868d7f758592bb4d10339a
davekolian <davekolian@users.noreply.github.com>
2020-09-10 21:07:40 +0200

    Added css and changed over-all layout

--- src/code/Main.java
+++ src/code/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -11,7 +12,8 @@ public class Main extends Application{
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("sudokuWindow.fxml"));
         stage.setTitle("Sudoku Solver");
-        stage.setScene(new Scene(root, 550, 640));
+        stage.setScene(new Scene(root, 635, 620));
+        stage.getIcons().add(new Image(Main.class.getResourceAsStream("sudoku.png")));
         stage.show();
     }
 
